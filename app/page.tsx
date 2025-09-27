cfba15b6dc31790b0d1f5978b1bbdce0e9036fd0
Alex Rebits <arebits@umich.edu>
2025-09-27 16:10:12 -0400

    Add nodes page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,13 +9,15 @@ export default function Home() {
         <p className="text-xl mt-4 ml-2">
           It's a place where curiosity is nurtured,
           <br />
-          Where you can explore new ideas, and
+          where you can explore new ideas, and
           <br />
-          Rediscover the joy of discovery.
+          rediscover the joy of discovery.
         </p>
-        <button className="hover:bg-[#1e00ff] hover:text-[#fff0d2] transition ml-2 mt-16 p-4 border-[#1e00ff] border-[1.5px] w-[60%] text-lg font-medium">
-          Take Me
-        </button>
+        <Link href="/nodes">
+          <button className="hover:bg-[#1e00ff] hover:text-[#fff0d2] transition ml-2 mt-16 p-4 border-[#1e00ff] border-[1.5px] w-[60%] text-lg font-medium">
+            Take Me
+          </button>
+        </Link>
         <div className="w-2 mt-0 ml-[20%] h-screen bg-gradient-to-b from-[#fff0d2] via-[#1e00ff] to-[#1e00ff]"></div>
       </div>
     </div>

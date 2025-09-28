cab7ef4cf7eb0828e45e95bb395eff96f6507c99
Shubham Patil <shubhampatilsd@gmail.com>
2025-09-28 12:01:42 -0400

    fuck

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,9 @@ import { PinoLogger } from "@mastra/loggers";
 import { LibSQLStore } from "@mastra/libsql";
 import { topicGeneratorAgent } from "./agents/topic-generator";
 import { enhancedTopicGeneratorAgent } from "./agents/enhanced-topic-generator";
-import { contentScraperTool } from "./tools/content-scraper";
 
 export const mastra = new Mastra({
   agents: { topicGeneratorAgent, enhancedTopicGeneratorAgent },
-  tools: { contentScraperTool },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
